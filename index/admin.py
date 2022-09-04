a19cde3eca178ca27956761d3f6615fc58783d06
KillNet <sbxdevops2@gmail.com>
2022-09-05 03:48:39 +0400

    Релизная версия сайта

--- index/admin.py
+++ index/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
+from ckeditor.widgets import CKEditorWidget
+from django import forms
+from index.models import SocialLink, Product, Contact, Answer, ProductImage, Category, PostBlog
 
-from index.models import SocialLink
 
-admin.site.register(SocialLink)
+class PostAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    max_num = 10
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline, ]
+    form = PostAdminForm
+
+
+class BlogAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = PostBlog
+        fields = '__all__'
+
+
+class BlogAdmin(admin.ModelAdmin):
+    form = BlogAdminForm
+
+
+admin.site.register(ProductImage, ProductImageAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(PostBlog, BlogAdmin)
+admin.site.register(Answer)
+admin.site.register(Category)
+admin.site.register(Contact)
--- index/views.py
+++ index/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
-from .models import SocialLink
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+
+from .forms import ContactForm
+from .models import SocialLink, Answer, Product, ProductImage, PostBlog
 
 
 class Index(ListView):
@@ -9,12 +11,54 @@ class Index(ListView):
 
     def get(self, request):
         social_link = SocialLink.objects.all()
+        answer = Answer.objects.all()
+        work = Product.objects.all()
 
         context = {
-            'social_link': social_link
+            'social_link': social_link,
+            'answer': answer,
+            'work': work,
         }
         return render(request, self.template_name, context)
 
 
-class Blog(TemplateView):
+class PortfolioDetail(DetailView):
+    """ Портфолио """
+    template_name = 'portfolio-details.html'
+    model = Product
+    context_object_name = 'product'
+
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        product_image = ProductImage.objects.filter(product=pk)
+
+        context = {
+            'product': product,
+            'product_image': product_image,
+        }
+        return render(request, self.template_name, context)
+
+
+class Blog(ListView):
     template_name = 'blog.html'
+    model = PostBlog
+    paginate_by = 3
+    context_object_name = 'post'
+
+
+class BlogDetail(DetailView):
+    template_name = 'blog-details.html'
+    model = PostBlog
+    context_object_name = 'post'
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index:index')
+    else:
+        form = ContactForm()
+
+    return render(request, 'index.html', {'form': form})
