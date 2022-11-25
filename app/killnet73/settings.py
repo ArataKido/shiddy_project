3863189253f87fa7a291347c3c154ac0ce1a1edc
killnet73 <killnet73@gmail.com>
2022-11-26 03:31:22 +0400

    fix

--- app/killnet73/settings.py
+++ app/killnet73/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8&4kgbog-e*r)f3jt1%s*9ezz!o%xfevp$+o-ap#zok_fvnrm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
